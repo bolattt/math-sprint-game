fc61d2f8f57644096b342304224201e0cfe60248
bolattt <bolattny@gmail.com>
2021-06-22 05:08:35 -0400

    populate DOM with data from equations array

--- script.js
+++ script.js
@@ -32,6 +32,12 @@ const wrongFormat = [];
 
 // Scroll
 
+// Display Game Page 
+function showGamePage(){
+  gamePage.hidden = false;
+  countdownPage.hidden = true;
+}
+
 // Get Random Number up to a max number 
 function getRandomInt(max){
   return Math.floor(Math.random() * Math.floor(max))
@@ -71,6 +77,21 @@ function createEquations() {
   }
   shuffle(equationsArray);
   console.log('equations array:',equationsArray)
+  equationsToDOM();
+}
+
+// Add Equations to DOM
+function equationsToDOM(){
+  equationsArray.forEach(equation => {
+    const item = document.createElement('div');
+    item.classList.add('item')
+    // Equation Text 
+    const equationText = document.createElement('h1')
+    equationText.textContent = equation.value;
+    // Append 
+    item.appendChild(equationText)
+    itemContainer.appendChild(item)
+  })
 }
 
 // Dynamically adding correct/incorrect equations
@@ -115,6 +136,9 @@ function showCountdown() {
   splashPage.hidden = true;
   countdownStart();
   createEquations();
+  setTimeout(() => {
+    showGamePage();
+  }, 4000);
 }
 
 // Get the value from selected radio button
